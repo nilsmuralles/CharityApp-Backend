1b50475fbbe0bf54f9bd677b839aec8b0a77088a
nilsmuralles <murallesnils11@gmail.com>
2025-04-27 20:04:54 -0600

    feat: Basic project boilerplate

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	router.Run(":8080")
+}
